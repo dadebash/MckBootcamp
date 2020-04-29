fdd57f4e814e4264f8417f1583b7814e3d98d96f
Debashis9012 <dedas@thoughtworks.com>
2020-04-30 00:14:47 +0530

    Program works fine! commeted as second program added

--- serializationSession/Program.cs
+++ serializationSession/Program.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.IO;
+﻿// using System;
+// using System.Runtime.Serialization;
+// using System.Runtime.Serialization.Formatters.Binary;
+// using System.Text;
+// using System.IO;
 
-namespace serializationSession
-{
+// namespace serializationSession
+// {
 
-    [Serializable]
-    class DigitalMarketing
-    {
-        public string customers;
-        public string socialNetwork;
-        static void Main(String[] args)
-        {
-            DigitalMarketing dm = new DigitalMarketing();
-            dm.customers = "Mark";
-            dm.socialNetwork = "Facebook";
+//     [Serializable]
+//     class DigitalMarketing
+//     {
+//         public string customers;
+//         public string socialNetwork;
+//         static void Main(String[] args)
+//         {
+//             DigitalMarketing dm = new DigitalMarketing();
+//             dm.customers = "Mark";
+//             dm.socialNetwork = "Facebook";
 
-            IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream("/Users/debashisdas/dadebash/Tutorial/MckBootcamp/serializationSession/ExampleNew.txt", FileMode.Create, FileAccess.Write);
-            formatter.Serialize(stream, dm);
-            stream.Close();
+//             IFormatter formatter = new BinaryFormatter();
+//             Stream stream = new FileStream("/Users/debashisdas/dadebash/Tutorial/MckBootcamp/serializationSession/ExampleNew.txt", FileMode.Create, FileAccess.Write);
+//             formatter.Serialize(stream, dm);
+//             stream.Close();
 
-            /* stream = new FileStream("/Users/debashisdas/dadebash/Tutorial/MckBootcamp/serializationSession/ExampleNew.txt", FileMode.Open, FileAccess.Read);
-            DigitalMarketing dmnewObj = (DigitalMarketing)formatter.Deserialize(stream);
-            Console.WriteLine(dmnewObj.customers);
-            Console.WriteLine(dmnewObj.socialNetwork);
-            Console.ReadKey();
-        } */
-    }
+//             /* stream = new FileStream("/Users/debashisdas/dadebash/Tutorial/MckBootcamp/serializationSession/ExampleNew.txt", FileMode.Open, FileAccess.Read);
+//             DigitalMarketing dmnewObj = (DigitalMarketing)formatter.Deserialize(stream);
+//             Console.WriteLine(dmnewObj.customers);
+//             Console.WriteLine(dmnewObj.socialNetwork);
+//             Console.ReadKey();
+//         } */
+//     }
 
 
-}
-}
+// }
+// }
